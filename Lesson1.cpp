e5ac4911558e9fb8a4e3ee035a98e596a022a4f5
Megudori <liam22541@gmail.com>
2025-11-15 12:24:03 +0700

    Add files via upload

---
+++ Lesson1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+  //This is a comment
+
+  /*
+    This
+    Is
+    a
+    multi-line
+    comment
+  */
+
+  std::cout << "I like Chicken!" << '\n';
+  std::cout << "It's super yummy!" << '\n';
+  std::cout << "Hello World!" << '\n';
+
+  return 0;
+}
---
+++ Variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(){
+
+  //Integers
+
+  int age = 21;
+  int year = 2025;
+  int days = 7;
+
+  //Assignment of integers
+  int x = 5; //early assignment
+  int y = 6;
+  int sum = x + y;
+
+  //double
+
+  double price = 10.99;
+  double gpa = 2.5;
+  double temperature = 25.1;
+
+  // single character
+  char grade = 'A';
+  char initial = 'B';
+  
+
+  std::cout << initial << '\n';
+
+
+  std::cout << price << '\n';
+
+  std::cout << x << '\n';
+  std::cout << y <<'\n';
+  std::cout << sum <<'\n';
+  std::cout << days <<'\n';
+  return 0;
+}
