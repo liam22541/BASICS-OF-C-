82c7b936f05e7d4de8a27f51b0f2aaa452027a8f
Megudori <liam22541@gmail.com>
2025-11-15 13:33:19 +0700

    Add files via upload

---
+++ Lesson1_Task.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+  // Integer
+  int friends = 5;
+
+  // String
+  std::string favorite = "I love eating food";
+  
+  // Double
+  double pi = 3.141592;
+
+  // Bolean
+  bool online = false;
+
+  std::cout << "I have " << friends << " friends" << '\n';
+
+  std::cout << favorite << '\n';
+
+  std::cout << pi;
+
+  std::cout << online;
+
+}
